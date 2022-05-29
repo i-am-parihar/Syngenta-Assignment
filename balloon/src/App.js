581567b11b6dce8f2e897414ed646bff027c1503
i-am-parihar <paritoshparihar4@gmail.com>
2022-05-29 15:32:18 +0530

    Update the Input Box

--- balloon/src/App.js
+++ balloon/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useEffect, useState} from "react" ;
+import {useState} from "react" ;
 import {useDispatch , useSelector} from "react-redux" ;
 import {randomColor } from './Redux/action';
 
@@ -17,46 +17,23 @@ function App() {
   }
   
   const handleChange = (e) => {
-  //  console.log(e.target.value) ;
-   setinputNumber(e.target.value-1) ;
-  //  var str = e.target.value ;
-  //  if(str.length>1){
-  //    alert("Please Enter Valid Number !") ;
-  //    return ;
-  //  }
-  //  if(Number(str[0])>5){
-  //    alert("Please Enter Valid Number !") ;
-  //    return ;
-  //  }
+    setinputNumber(e.target.value-1) ;
   }
   const handleShoot = () => {
     let ele = data.splice(inputNumber , 1) ;
-    let obj = {
-      id : ele[0].id ,
-      eleColor : ele[0].color ,
-    }
-    console.log(ele) ;
-    console.log(obj) ;
+    let obj = {id : ele[0].id ,eleColor : ele[0].color} ;
     let arr = [...emptyArr , obj] ;
     setEmptyArr(arr) ;
-    // emptyArr.push(obj) ;
     setToggle(!toggle) ;
   }
   const handlePushtoMain = (index) => {
     let ele = emptyArr.splice(index , 1) ;
-    let obj = {
-      id : ele[0].id ,
-      color : ele[0].eleColor ,
-    }
-    console.log("obj" , obj) 
-    console.log("ele" , ele) 
+    let obj = {id : ele[0].id ,color : ele[0].eleColor} ;
     data.push(obj) ;
     setData(data.sort((a,b) => a.id - b.id)) ;
-  //  setData(demo) ;
     setToggle(!toggle) ;
   }
-// console.log("dataOuter" ,data) ;
-// console.log("emptyArr" , emptyArr)
+
   return (
     <div className="App">
       <div className="emptyDiv">
@@ -83,7 +60,7 @@ function App() {
 
       <div className="btnDiv">
         <h4>Button</h4>
-        <input onChange={(e) => handleChange(e)}></input><br/>
+        <input type="number" onChange={(e) => handleChange(e)}></input><br/>
         <button onClick={() => handleShoot()}>shoot</button>
       </div>
     </div>
